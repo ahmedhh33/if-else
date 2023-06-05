335d256f2336126cadc4be862aa8cb1cbfa573ce
ahmedhh33 <ahmedhh3633@gmail.com>
2023-06-05 11:30:22 +0400

    nested loop

--- Program.cs
+++ Program.cs
@@ -4,41 +4,55 @@
     {
         static void Main(string[] args)
         {
-            int a = 1, b = 1;
-            //bool c;
-            int c = 5;
-            //Console.WriteLine(c);
+            //int a = 1, b = 1;
+            ////bool c;
+            //int c = 5;
+            ////Console.WriteLine(c);
 
-            if(a<b || c==5) // must one condition true two extcude 
-            {
-                Console.WriteLine("condition met ");
-            }
-            //else if(a>b)
+            //if(a<b || c==5) // must one condition true two extcude 
             //{
-            //    Console.WriteLine("if else condition met ");
+            //    Console.WriteLine("condition met ");
             //}
-            else
-            {
-                Console.WriteLine(" else condition met ");
-            }
+            ////else if(a>b)
+            ////{
+            ////    Console.WriteLine("if else condition met ");
+            ////}
+            //else
+            //{
+            //    Console.WriteLine(" else condition met ");
+            //}
 
-            if (a < b && c == 5) // must two condition true two extcude 
-            {
-                Console.WriteLine("and condition met ");
-            }
-            else
-            {
-                Console.WriteLine(" and condition not met ");
-            }
+            //if (a < b && c == 5) // must two condition true two extcude 
+            //{
+            //    Console.WriteLine("and condition met ");
+            //}
+            //else
+            //{
+            //    Console.WriteLine(" and condition not met ");
+            //}
 
-            Console.WriteLine("conditions types");
-            int t=1,y=4,z=5;
-            if (t<y && y<=z)
+            //Console.WriteLine("conditions types");
+            //int t=1,y=4,z=5;
+            //if (t<y && y<=z)
+            //{
+            //    Console.WriteLine("that is great ");
+            //}
+            //else
+            //{ Console.WriteLine("try ageen "); }
+
+
+            Console.WriteLine("Nested loops");
+            for(int i=0; i<5; i++)
             {
-                Console.WriteLine("that is great ");
+                Console.WriteLine("i body ");
+                Console.WriteLine(" i = " + i);
+                for(int j=0; j<5; j++)
+                {
+                   // Console.WriteLine("j body ");
+                    Console.WriteLine(" j = " + j);
+                }
+                Console.WriteLine(" after j loop");
             }
-            else
-            { Console.WriteLine("try ageen "); }
         }
     }
 }
