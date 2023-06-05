2174c631ee72bcaa217cab53af5d1ebabf4d0f97
ahmedhh33 <ahmedhh3633@gmail.com>
2023-06-05 14:54:04 +0400

    program that takes two inputs from users ard prints all even and odd numbers

--- Program.cs
+++ Program.cs
@@ -41,25 +41,50 @@
             //{ Console.WriteLine("try ageen "); }
 
 
-            Console.WriteLine("Nested loops");
-            for(int i=0; i<5; i++)
+            //Console.WriteLine("Nested loops");
+            //for(int i=0; i<5; i++)
+            //{
+            //    Console.WriteLine("i body ");
+            //    Console.WriteLine(" i = " + i);
+            //    for(int j=0; j<5; j++)
+            //    {
+            //       //Console.WriteLine("j body ");
+            //        Console.WriteLine(" j = " + j);
+            //    }
+            //    Console.WriteLine(" after j loop");
+            //}
+            //Console.WriteLine(" Anuther nested loop: ");
+            //for(int i=0; i<5;i++)
+            //{
+            //    for(int j=0; j<i;j++)
+            //    {
+            //        Console.WriteLine(j);
+            //    }
+            //}
+            Console.WriteLine("program that takes two inputs from users ard prints all even and odd numbers ");
+            Console.WriteLine("Enter the vale of starting number = ");
+            int st = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the vale of ending number = ");
+            int en = int.Parse(Console.ReadLine());
+            Console.WriteLine("******************");
+            for (int i = st; i <= en; i++)
             {
-                Console.WriteLine("i body ");
-                Console.WriteLine(" i = " + i);
-                for(int j=0; j<5; j++)
+                //Console.WriteLine((2*i)+1);
+                if (i % 2 != 0)
                 {
-                   // Console.WriteLine("j body ");
-                    Console.WriteLine(" j = " + j);
+                    Console.WriteLine("Odd - " + i);
                 }
-                Console.WriteLine(" after j loop");
+                else
+                    Console.WriteLine("Even - " + i);
             }
-            Console.WriteLine(" Anuther nested loop: ");
-            for(int i=0; i<5;i++)
+            for (int i = 1; i <= 5; i++)
             {
-                for(int j=0; j<i;j++)
+                for (int j = 1; j <= i; j++)
                 {
-                    Console.WriteLine(j);
+                    Console.Write("*");
                 }
+
+                Console.WriteLine();
             }
         }
     }
