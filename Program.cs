37419f8cf4fdbfbb3d7c8079112586ea5aacd7c2
ahmedhh33 <ahmedhh3633@gmail.com>
2023-06-05 09:56:57 +0400

    if-else

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0, b = 1;
+            bool c;
+            c = a == b;
+            Console.WriteLine(c);
+
+            if(a!=b)
+            {
+                Console.WriteLine("condition met ");
+            }
+            else if(a>b)
+            {
+                Console.WriteLine("if else condition met ");
+            }
+            else
+            {
+                Console.WriteLine(" else condition met ");
+            }
+        }
+    }
+}
